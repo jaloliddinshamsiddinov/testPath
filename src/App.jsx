7e84eeaf230cd4e5475313019c85ecca82c5e0bb
Jaloliddin <hi.shamsiddinov@gmail.com>
2024-07-15 00:15:07 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import png from './assets/png/img.webp'
 const App = () => {
   return (
     <div>
-      <img src="src/assets/svg/hero.svg" alt="src" />
+      <img src="/assets/svg/hero.svg" alt="/assets" />
       <img src="/src/assets/svg/hero.svg" alt="/src" />
       <img src="./src/assets/svg/hero.svg" alt="./src" />
       <img src={svg} alt="import" />
       <p>png images</p>
-      <img src="src\assets\png\img.webp" alt="src" />
+      <img src="\assets\png\img.webp" alt="/assets" />
       <img src="/src\assets\png\img.webp" alt="/src" />
       <img src="./src\assets\png\img.webp" alt="./src" />
       <img src={png} alt="import" />
