d0626286eec1c296367f9a2fd92d96ef804dd2e0
Jaloliddin <hi.shamsiddinov@gmail.com>
2024-07-15 00:13:20 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import svg from './assets/svg/svg.svg'
+import png from './assets/png/img.webp'
 
 const App = () => {
   return (
     <div>
-      <img src="src/assets/svg/hero.svg" alt="src/assets/svg/hero.svg" />
-      <img src="/src/assets/svg/hero.svg" alt="/src/assets/svg/hero.svg" />
-      <img src="./src/assets/svg/hero.svg" alt="./src/assets/svg/hero.svg" />
-      <img src="./assets/svg/hero.svg" alt="./assets/svg/hero.svg" />
+      <img src="src/assets/svg/hero.svg" alt="src" />
+      <img src="/src/assets/svg/hero.svg" alt="/src" />
+      <img src="./src/assets/svg/hero.svg" alt="./src" />
+      <img src={svg} alt="import" />
       <p>png images</p>
-      <img src="src\assets\png\img.webp" alt="src/assets/svg/hero.svg" />
-      <img src="/src\assets\png\img.webp" alt="/src\assets\png\img.webp" />
-      <img src="./src\assets\png\img.webp" alt="./src\assets\png\img.webp" />
+      <img src="src\assets\png\img.webp" alt="src" />
+      <img src="/src\assets\png\img.webp" alt="/src" />
+      <img src="./src\assets\png\img.webp" alt="./src" />
+      <img src={png} alt="import" />
     </div>
   )
 }
